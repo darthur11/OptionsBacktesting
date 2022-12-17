8ef14a46091b8e97ff4cc9037d85ac4b22f16d76
Artur.Dossatayev <artur.dossatayev@coins.ph>
2022-12-17 22:02:19 +0600

    separate volatility_hedge_strat

--- source/volatility_hedge_strategy.py
+++ source/volatility_hedge_strategy.py
@@ -12,29 +12,33 @@ class VolatilityHedgeStrategy(Strategy):
 
     def close_strat(self):
         num_open_positions = self.get_num_open_positions()
-        if (num_open_positions > 0 and self.relative_profit > 0.9):
+        revenue, volume, position_open_for = self.get_position_metrics()
+        relative_profit = revenue / volume if volume > 0 else 0
+        if (num_open_positions > 0 and relative_profit > 0.9):
             return 1
-        elif (self.open_for > 8 and self.relative_profit < -0.2):
+        elif (position_open_for > 8 and relative_profit < -0.2):
             return 1
-        elif (num_open_positions > 0 and self.relative_profit > .5 and self.min_dte < 9):
+        elif (num_open_positions > 0 and relative_profit > .5 and self.get_min_dte() < 9):
             return 1
-        elif (num_open_positions > 0 and self.min_dte < 6):
+        elif (num_open_positions > 0 and self.get_min_dte() < 6):
             return 1
         else:
             return 0
 
     def get_instruments_for_strategy(self, cash_available):
+        first_leg, second_leg = [], []
         expirations = self.options_chain.items()
         try:
             first_leg = [con for con in expirations if (con[1][2] == 1 and con[1][3] == 1)][0]
+            print("first leg: ", first_leg)
         except:
             print("no first leg")
         try:
             second_leg = [con for con in expirations if (con[1][2] == 1 and con[1][3] == 2)][0]
+            print("second leg: ", second_leg)
         except:
             print("no second leg")
-        print("first leg: ", first_leg)
-        print("second leg: ", second_leg)
+
         if len(first_leg) > 0 and len(second_leg) > 0:
             amount = round(0.2 * cash_available / (-first_leg[1][0] + second_leg[1][0]), -3)
             print(amount)
